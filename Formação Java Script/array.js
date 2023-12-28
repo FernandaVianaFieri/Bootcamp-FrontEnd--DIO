df31533ffc6b5c769833b657939acb5c773ccaf9
Fernanda_Fer <83472001+FernandaVianaFieri@users.noreply.github.com>
2023-12-28 20:00:46 -0300

    Add files via upload

---
+++ Formação Java Script/array.js
@@ -0,0 +1,32 @@
+
+
+
+let nomeBeneficiario= ['Marcelo', 'Fabio', 'Sabrina',];
+let valorConta = [500,300,1000];
+let dataVencimento = ['04/12/2023.','14/01/2023.','31/12/2023'];
+
+
+for (let i = 0;  i < nomeBeneficiario.length;i++) {
+  
+ }     
+
+for (let i = 0;  i< valorConta.length; i++) {  
+       
+  }
+  
+  for (let i = 0;  i< dataVencimento.length; i++) {
+    console.log(nomeBeneficiario[i] + ' >>'+ 'Pagamento Agendado! Valor: R$'+ valorConta[i] + ',' + 'vencimento ' + dataVencimento[i]);
+    
+  }
+
+
+
+  
+
+ 
+ 
+
+
+
+
+
